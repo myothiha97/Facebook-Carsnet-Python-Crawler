79744af6ae9816a77f8c3cd8f57d56a261baf545
thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2020-07-28 15:29:45 +0630

    Update click see_more action for group

--- FacebookPostAction.py
+++ FacebookPostAction.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium import webdriver
 
-def click_see_more_button(browser,post):
+def click_see_more_button(browser,post,type):
+    if type == 0:
         try:
             # seemore = post.find_element_by_xpath("//div[contains(text(),'See more')]")
             seemore = post.find_element_by_css_selector('div.j83agx80.cbu4d94t.ew0dbk1b.irj2b8pg > div > span > div > div:last-child > div')
@@ -13,3 +14,14 @@ def click_see_more_button(browser,post):
             time.sleep(1)
         except Exception as e:
             print("An error occur while trying to click see_more button : ",str(e))
+
+    elif type == 1:
+        try:
+            seemore = post.find_element_by_css_selector("div.dati1w0a.f10w8fjw.hv4rvrfc.jb3vyjys > span > div:last-child > div")
+            time.sleep(.5)
+            webdriver.ActionChains(browser).move_to_element(seemore).perform()
+            browser.execute_script("arguments[0].click();", seemore)
+            time.sleep(1)
+        except Exception as e:
+            print("An error occur while trying to click see_more button : ",str(e))
+        
